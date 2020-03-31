7de4ec6d4e416b49a8bda2c2e8c807d6167979cf
kushkaftar <kushkaftar@gmail.com>
2020-04-01 01:01:46 +0300

    add lesson 3++

--- src/script.js
+++ src/script.js
@@ -1,41 +1,45 @@
 //третье доп. зпдание
+//переменные
+let form  = document.getElementsByTagName('form');
+let language = document.getElementById('icon_prefix');
+let firstName = document.getElementById('icon_prefix2');
+let lang = null;
 
-//получаем значение переменной
-let lang = prompt("введите ru или en");
+language.addEventListener("input",  () => {validLang(language)});
 
-//блок if
-if (lang === 'ru') {
-    console.log('пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс');
-} else if (lang === 'en') {
-    console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
-} else {
-    console.log('not ru or en');
-}
+const validLang = (input) => {
 
-//блок switch
-switch (lang) {
-    case 'ru':
-        console.log('пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс');
-        break;
-    case  'en':
-        console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
-        break;
-    default:
-        alert('not ru or en');
-}
+    if (input.value === 'ru' || input.value === 'en') {
+        lang = input.value;
 
-//блок с массивом
-let arrLang = {
-    'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
-    'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+        //блок if
+        if (lang === 'ru') {
+            console.log('пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс');
+        } else if (lang === 'en') {
+            console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
+        } else {
+            console.log('not ru or en');
+        }
+        //блок switch
+        switch (lang) {
+            case 'ru':
+                console.log('пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс');
+                break;
+            case  'en':
+                console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
+                break;
+            default:
+                alert('not ru or en');
+        }
+    }
 };
-console.log(arrLang[lang]);
 
-//тернарный опреатор
+firstName.addEventListener("input",  () => {addName(firstName)});
 
-//получаем значение переменной
-let namePerson = prompt("введите имя");
-
-let position = namePerson === 'Артем' ? "директор" : namePerson === 'Максим' ? "преподаватель" : "студент";
-
-console.log(position);
+const addName = (input) => {
+    if (input.value !== '') {
+        let namePerson = input.value;
+        let position = namePerson === 'Артем' ? "директор" : namePerson === 'Максим' ? "преподаватель" : "студент";
+        console.log(position);
+    }
+};
