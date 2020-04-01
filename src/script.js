4bd53c0adfec5b47c22e2dab9bd4315560450381
kushkaftar <kushkaftar@gmail.com>
2020-04-01 22:38:36 +0300

    lesson 4+

--- src/script.js
+++ src/script.js
@@ -1,45 +1,19 @@
-//третье доп. зпдание
-//переменные
-let form  = document.getElementsByTagName('form');
-let language = document.getElementById('icon_prefix');
-let firstName = document.getElementById('icon_prefix2');
-let lang = null;
+//4 task
 
-language.addEventListener("input",  () => {validLang(language)});
+const f = function (arg) {
+    let message;
 
-const validLang = (input) => {
-
-    if (input.value === 'ru' || input.value === 'en') {
-        lang = input.value;
-
-        //блок if
-        if (lang === 'ru') {
-            console.log('пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс');
-        } else if (lang === 'en') {
-            console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
-        } else {
-            console.log('not ru or en');
+    if (typeof (arg) === 'string') {
+        message = arg.trim();
+        if (message.length > 30) {
+            message = message.slice(0, 30) + ' ...';
         }
-        //блок switch
-        switch (lang) {
-            case 'ru':
-                console.log('пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс');
-                break;
-            case  'en':
-                console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
-                break;
-            default:
-                alert('not ru or en');
-        }
+        return message;
     }
+    message = 'arg not string';
+    return message;
 };
 
-firstName.addEventListener("input",  () => {addName(firstName)});
-
-const addName = (input) => {
-    if (input.value !== '') {
-        let namePerson = input.value;
-        let position = namePerson === 'Артем' ? "директор" : namePerson === 'Максим' ? "преподаватель" : "студент";
-        console.log(position);
-    }
-};
+console.log(f(5));
+console.log(f('string'));
+console.log(f('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'));
