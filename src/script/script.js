e9eb62a80db44e506cac59035e6e7ed063088952
kushkaftar <kushkaftar@gmail.com>
2020-05-06 00:28:17 +0300

    l16

--- src/script/script.js
+++ src/script/script.js
@@ -1,45 +1,17 @@
 'use strict';
 
-const app = document.querySelector('#app');
-console.log(app);
-
-const DomElement = function () {
-    this.selector = "";
-    this.height = "200px";
-    this.width = "600px";
-    this.bg = "green";
-    this.fontSize = "26px";
-};
-
-DomElement.prototype.addElem = function (str) {
-    console.log(str);
-    switch (str[0]) {
-        case ".":
-            let div = document.createElement("div");
-            div.setAttribute("class", str.slice(1));
-            div.innerText = "div";
-            div.style.height = this.height;
-            div.style.width = this.width;
-            div.style.backgroundColor = this.bg;
-            div.style.fontSize = this.fontSize;
-            app.append(div);
-            console.log(div);
-            break;
-        case "#":
-            let p = document.createElement("p");
-            p.setAttribute("id", str.slice(1));
-            p.innerText = "p";
-            p.style.height = this.height;
-            p.style.width = this.width;
-            p.style.backgroundColor = this.bg;
-            p.style.fontSize = this.fontSize;
-            app.append(p);
-            console.log(p);
-            break;
+class First {
+    sayHello() {
+        console.log(`Привет я метод родителя!`);
     }
-};
+}
 
-const domElement = new DomElement();
+class Second  extends First {
+    sayHelloTo() {
+        super.sayHello();
+        console.log(`А я наследуемый метод!`);
+    }
+}
 
-domElement.addElem(".qqq");
-domElement.addElem("#qqq");
+const second = new Second();
+second.sayHelloTo();
